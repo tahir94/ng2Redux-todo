77e74bc17f52518ffe48ac89168aa1f268617985
tahir94 <tahirkhan.on@gmail.com>
2017-08-28 20:06:27 +0500

    conflicts removed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,63 +4,85 @@ import { IAppState } from "../store2"
 import { TODO_SUBMIT } from "../actions";
 import { EDIT_TODO } from "../actions";
 import { UPDATE_TODO } from "../actions";
+import { REMOVE_TODO } from "../actions";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-  isUpdate;
-  selectedUserTodo;
-  isEdited: boolean;
+export class HomeComponent implements OnInit {
+	// isUpdate;
+	selectedUserTodo;
+	isEdited: boolean;
+	isEdit_Remove : boolean = true;
+	userTodo : Object;
 
-  @select('todos') todo;
-  @select((s: IAppState) =>  s.currentUserTodo) todoInput
-  constructor(private ngRedux: NgRedux<IAppState>) {
-    this.ngRedux.subscribe(() => {
-      console.log(ngRedux.getState());
-      this.selectedUserTodo = ngRedux.getState().currentUserTodo;
+	@select('todos') todo;
+	@select((s: IAppState) => s.currentUserTodo) todoInput
+	constructor(private ngRedux: NgRedux<IAppState>) {
+		this.ngRedux.subscribe(() => {
+			console.log(ngRedux.getState());
+			this.selectedUserTodo = ngRedux.getState().currentUserTodo;
 
-    })
-  }
+		})
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
-  submit(userTodo) {
-    this.ngRedux.dispatch({
-      type: TODO_SUBMIT,
-      payload: userTodo
-    })
-  }
+	
+	isDisabled() {
+		if (this.userTodo) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 
-  edit(todo, index) {
-    this.isEdited = true;
+	submit(userTodo) {
+		// this.userTodo = userTodo
+		this.ngRedux.dispatch({
+			type: TODO_SUBMIT,
+			payload: userTodo
+		})
+		this.userTodo = '';
+	}
 
-    this.ngRedux.dispatch({
-      type: EDIT_TODO,
-      payload: todo,
-      indexNum: index,
-      selectedTodo: this.selectedUserTodo,
-      isEdit: this.isEdited
+	edit(todo, index) {
+		this.isEdit_Remove = false;
+		this.isEdited = true;
 
-    })
+		this.ngRedux.dispatch({
+			type: EDIT_TODO,
+			payload: todo,
+			indexNum: index,
+			selectedTodo: this.selectedUserTodo,
+			isEdit: this.isEdited
 
-  }
-  // onChange(value){
-  // console.log(value);
-  
-  // }
+		})
 
-  update(updatedTodo,index){
-    this.ngRedux.dispatch({
-      type : UPDATE_TODO,
-      payload : updatedTodo,
-      indexNum : index
-    })
-    
-  }
+	}
+
+
+
+	update(updatedTodo, index) {
+		this.isEdit_Remove = true;
+		this.isEdited = false;
+		this.ngRedux.dispatch({
+			type: UPDATE_TODO,
+			payload: updatedTodo,
+			indexNum: index
+		})
+
+	}
+
+	remove(todo, index) {
+		this.ngRedux.dispatch({
+			type: REMOVE_TODO,
+			indexNum: index
+		})
+	}
 
 
 }
